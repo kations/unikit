5d72e207d9f642ea3424012ef8c98ad1bf642399
your_name <your_email>
2019-09-12 22:04:09 +0200

    deploy test

--- .docz/.cache/async-requires.js
+++ .docz/.cache/async-requires.js
@@ -2,15 +2,14 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/kations/Projekte/unikit/.docz/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/Users/kations/Projekte/unikit/.docz/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-index-mdx": () => import("/Users/kations/Projekte/unikit/src/index.mdx" /* webpackChunkName: "component---src-index-mdx" */),
   "component---src-box-box-mdx": () => import("/Users/kations/Projekte/unikit/src/Box/box.mdx" /* webpackChunkName: "component---src-box-box-mdx" */),
   "component---src-button-button-mdx": () => import("/Users/kations/Projekte/unikit/src/Button/button.mdx" /* webpackChunkName: "component---src-button-button-mdx" */),
+  "component---src-flex-flex-mdx": () => import("/Users/kations/Projekte/unikit/src/Flex/flex.mdx" /* webpackChunkName: "component---src-flex-flex-mdx" */),
   "component---src-alert-alert-mdx": () => import("/Users/kations/Projekte/unikit/src/Alert/alert.mdx" /* webpackChunkName: "component---src-alert-alert-mdx" */),
+  "component---src-progress-index-mdx": () => import("/Users/kations/Projekte/unikit/src/Progress/index.mdx" /* webpackChunkName: "component---src-progress-index-mdx" */),
   "component---src-avatar-avatar-mdx": () => import("/Users/kations/Projekte/unikit/src/Avatar/avatar.mdx" /* webpackChunkName: "component---src-avatar-avatar-mdx" */),
   "component---src-button-group-button-group-mdx": () => import("/Users/kations/Projekte/unikit/src/ButtonGroup/ButtonGroup.mdx" /* webpackChunkName: "component---src-button-group-button-group-mdx" */),
-  "component---src-flex-flex-mdx": () => import("/Users/kations/Projekte/unikit/src/Flex/flex.mdx" /* webpackChunkName: "component---src-flex-flex-mdx" */),
-  "component---src-progress-index-mdx": () => import("/Users/kations/Projekte/unikit/src/Progress/index.mdx" /* webpackChunkName: "component---src-progress-index-mdx" */),
-  "component---src-index-mdx": () => import("/Users/kations/Projekte/unikit/src/index.mdx" /* webpackChunkName: "component---src-index-mdx" */)
+  "component---src-pages-404-js": () => import("/Users/kations/Projekte/unikit/.docz/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
 }
 
--- .docz/.cache/sync-requires.js
+++ .docz/.cache/sync-requires.js
@@ -5,15 +5,14 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/kations/Projekte/unikit/.docz/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/kations/Projekte/unikit/.docz/src/pages/404.js"))),
+  "component---src-index-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/index.mdx"))),
   "component---src-box-box-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/Box/box.mdx"))),
   "component---src-button-button-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/Button/button.mdx"))),
+  "component---src-flex-flex-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/Flex/flex.mdx"))),
   "component---src-alert-alert-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/Alert/alert.mdx"))),
+  "component---src-progress-index-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/Progress/index.mdx"))),
   "component---src-avatar-avatar-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/Avatar/avatar.mdx"))),
   "component---src-button-group-button-group-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/ButtonGroup/ButtonGroup.mdx"))),
-  "component---src-flex-flex-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/Flex/flex.mdx"))),
-  "component---src-progress-index-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/Progress/index.mdx"))),
-  "component---src-index-mdx": hot(preferDefault(require("/Users/kations/Projekte/unikit/src/index.mdx")))
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/kations/Projekte/unikit/.docz/src/pages/404.js")))
 }
 
